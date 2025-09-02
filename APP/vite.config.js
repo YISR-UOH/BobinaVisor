322bfc0826a7fc849f9ca9a7a779cf353b56b153
Yerko Sepulveda Rojas <yerko.sepulveda@pregrado.uoh.cl>
2025-09-02 12:40:15 -0400

    refactor: format Vite configuration for better readability

--- APP/vite.config.js
+++ APP/vite.config.js
@@ -6,5 +6,14 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/BobinaVisor/",
-  plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MiB
+      },
+    }),
+  ],
 });
